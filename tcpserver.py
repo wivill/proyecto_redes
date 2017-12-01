caed470e93079788fd2b1c0ff66225bd91cf105c
Willy <gopwivill@gmail.com>
2017-12-01 15:00:10 -0600

    minúsculas a mayúsculas

--- tcpserver.py
+++ tcpserver.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 
 # Importa la biblioteca socket para crear enlaces
 import socket
@@ -14,10 +15,10 @@ s.listen(5)
 while True:
     c, addr = s.accept()
     data = c.recv(1024)
-
+    data_return = data.upper()
 # Agregar conversión de minúsculas a mayúsculas
 
-    print('Address:', addr, 'Data:', data)
+    print('Address:', addr, 'Data:', data_return)
 
     # mylist = list(data.split(':'))
     mylist = list(data.split(' '))
--- udpserver.py
+++ udpserver.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 22222))
 while True:
     data, addr = s.recvfrom(1024)
-    print('Address:', addr, 'Data:', data)
+    data_return = data.upper()
+    print('Address:', addr, 'Data:', data_return)
     mylist = list(data.split(':'))
     # intlist = list()
     # for i in range(0, len(mylist)):
