0d38e3915454b434a1049a2e41f9fece955fd569
BernardoZuniga <bjza09@hotmail.com>
2017-12-02 18:40:53 -0600

    Se pueden enviar varias tramas

--- tcpclient.py
+++ tcpclient.py
@@ -4,6 +4,8 @@
 import sys
 import socket
 
+
+
 arglen = len(sys.argv)
 if arglen < 3:
     print('Ejecute como python tcpclient.py <ip_address> <numbers>')
@@ -12,17 +14,38 @@ data = str()
 data = data + str(sys.argv[2])
 for i in range(3, arglen):
     data = data + ' ' + str(sys.argv[i])
-print("Datos enviados")
-print(data)
 
-s = socket.socket()
 
-port = 11111
-s.connect((sys.argv[1], port))
-s.send(data)
+data = str()
+data = data + str(sys.argv[2])
+for i in range(3, arglen):
+	data = data + ' ' + str(sys.argv[i])
 
-print("\nDatos recibidos")
-print(s.recv(1024))
+bandera = 0   
+while bandera == 0:
+	
+	print("Datos enviados")
+	print(data)
+	
+	s = socket.socket()
+	port = 11111
+	s.connect((sys.argv[1], port))
+	s.send(data)
+	
+	print("\nDatos recibidos")
+	datos_recibidos = s.recv(1024)
+	print(datos_recibidos)
+	datos_recibidos = str()
+	
+	if datos_recibidos == "-*/D":
+		print("if")
+		bandera = 1
+	else:
+		data = str(raw_input('\nIntroduzca el dato nuevamente. Para finalizar introduzca "-*/d" '))
+	
+s.shutdown(0)
+s.close
+	
 
 
 h= socket.socket()
@@ -34,5 +57,4 @@ h.send(data)
 
 print("\nDatos enviados")
 print(data)
-s.shutdown(0)
-s.close
+
